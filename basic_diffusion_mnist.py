705909cd9ebaf34445b53ade08fc71ce48bd7b31
Nixxx19 <nixxspeaks@gmail.com>
2025-10-19 23:27:58 +0530

    dataset

--- basic_diffusion_mnist.py
+++ basic_diffusion_mnist.py
@@ -5,3 +5,17 @@ from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 import matplotlib.pyplot as plt
 from tqdm import tqdm
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+epochs = 5
+batch_size = 128
+T = 300  # number of diffusion steps
+lr = 1e-4
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,))
+])
+
+data = datasets.MNIST(root='./data', download=True, transform=transform)
+dataloader = DataLoader(data, batch_size=batch_size, shuffle=True)
